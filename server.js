5b4c869208447c0653e33a03faf62bb3b4f32c11
wyl350 <wyl350@126.com>
2021-09-20 21:39:37 +0800

    +

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const ronin = require('ronin-server')
-const mocks = require('ronin-mocks')
-
+const ronin     = require( 'ronin-server' )
+const mocks     = require( 'ronin-mocks' )
+const database  = require( 'ronin-database' )
 const server = ronin.server()
 
-server.use('/', mocks.server(server.Router(), false, true))
-server.start()
+ server.use( '/foo', (req, res) => {
+   return res.json({ "foo": "bar" })
+ })
 
+database.connect( process.env.CONNECTIONSTRING )
+server.use( '/', mocks.server( server.Router(), false, false ) )
+server.start()
